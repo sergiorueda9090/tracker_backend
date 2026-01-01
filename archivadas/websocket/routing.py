49c88ec0894018ec5406c6918c77777ba8d8ec19
sergiorueda9090 <sergiorueda90@hotmail.com>
2026-01-01 08:55:55 -0500

    modulo archivadas

--- archivadas/websocket/routing.py
+++ archivadas/websocket/routing.py
@@ -1,7 +1,7 @@
-# Archivada/websocket/routing.py
+# Archivadas/websocket/routing.py
 from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/archivada/$', consumers.ArchivadaConsumer.as_asgi()),
+    re_path(r'ws/archivadas/$', consumers.ArchivadaConsumer.as_asgi()),
 ]
